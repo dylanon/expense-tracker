2d1853f24bbaccd29cef157d5a4d7c6298bbc90c
Dylan On <dylanon@users.noreply.github.com>
2019-12-08 10:23:49 -0500

    fix: 🐛 fix missing /up endpoint

--- api/routes/public/index.js
+++ api/routes/public/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express')
 const router = new Router()
 
 const token = require('./token')
+const up = require('./up')
 const users = require('./users')
 
 router.use('/token', token)
+router.use('/up', up)
 router.use('/users', users)
 
 module.exports = router
