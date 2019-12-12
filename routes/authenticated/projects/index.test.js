4427a25fe74e7c6e2c1039b9b5c25ee9fb48b0bc
Dylan On <dylanon@users.noreply.github.com>
2019-12-12 11:38:59 -0500

    test: 💍 read, update, delete, more create tests for /projects

--- routes/authenticated/projects/index.test.js
+++ routes/authenticated/projects/index.test.js
@@ -29,21 +29,6 @@ describe('with authentication', () => {
     await user.destroy()
   })
 
-  it('creates a project', async () => {
-    const projectName = 'Test Project'
-    const { body } = await requestWithAuth
-      .post('/projects')
-      .send({ name: projectName })
-      .expect(201)
-    expect(body).toEqual(
-      expect.objectContaining({
-        id: expect.any(String),
-        name: projectName,
-        createdBy: expect.any(String),
-      })
-    )
-  })
-
   it('lists projects', async () => {
     const projectNames = [
       'Project One',
@@ -62,6 +47,173 @@ describe('with authentication', () => {
     const { body } = await requestWithAuth.get('/projects').expect(200)
     body.forEach(returnedProject => {
       expect(expectedProjects).toContainEqual(returnedProject)
+    })
+  })
+
+  describe('create', () => {
+    it('creates a project without a name', async () => {
+      const { body } = await requestWithAuth.post('/projects').expect(201)
+      expect(body).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          name: null,
+          createdBy: expect.any(String),
+        })
+      )
+    })
+
+    it('creates a project with a name', async () => {
+      const projectName = 'Test Project'
+      const { body } = await requestWithAuth
+        .post('/projects')
+        .send({ name: projectName })
+        .expect(201)
+      expect(body).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          name: projectName,
+          createdBy: expect.any(String),
+        })
+      )
+    })
+
+    it('prevents creation a project with invalid attributes', async () => {
+      const projectName = 'Test Project 2'
+      const createWithCreatedBy = requestWithAuth
+        .post('/projects')
+        .send({ name: projectName, createdBy: user.id })
+      const createWithBlah = requestWithAuth
+        .post('/projects')
+        .send({ blah: 2019 })
+      const responses = await Promise.all([createWithCreatedBy, createWithBlah])
+      responses.forEach(response => {
+        const {
+          status,
+          body: { id, errors },
+        } = response
+        expect(status).toBe(400)
+        expect(errors).toEqual(expect.any(Array))
+        expect(id).toBeUndefined()
+      })
+    })
+  })
+
+  describe('read', () => {
+    it('reads a project by id', async () => {
+      const projectName = 'Project Z'
+      const project = new ProjectFixture({
+        name: projectName,
+        createdBy: user.id,
+      })
+      const projectAttributes = await project.create()
+      const { body } = await requestWithAuth
+        .get(`/projects/${projectAttributes.id}`)
+        .expect(200)
+      expect(body).toEqual(projectAttributes)
+    })
+
+    it('responds with 404 when the project is not found', async () => {
+      const {
+        body: { errors },
+      } = await requestWithAuth.get(`/projects/1`).expect(404)
+      expect(errors).toEqual(expect.any(Array))
+    })
+  })
+
+  describe('update', () => {
+    it('updates a project name', async () => {
+      const originalProjectName = 'Project A'
+      const newProjectName = 'Project B'
+      const project = new ProjectFixture({
+        name: originalProjectName,
+        createdBy: user.id,
+      })
+      const { id: projectId } = await project.create()
+      const { body } = await requestWithAuth
+        .patch(`/projects/${projectId}`)
+        .send({
+          name: newProjectName,
+        })
+        .expect(200)
+      expect(newProjectName).not.toBe(originalProjectName)
+      expect(body.id).toBe(projectId)
+      expect(body.name).toBe(newProjectName)
+    })
+
+    it('prevents updating invalid properties', async () => {
+      const project = new ProjectFixture({
+        name: 'Project Y',
+        createdBy: user.id,
+      })
+      const { id: projectId } = await project.create()
+
+      const updateCreatedBy = requestWithAuth
+        .patch(`/projects/${projectId}`)
+        .send({
+          createdBy: user.id,
+        })
+      const updateBlah = requestWithAuth.patch(`/projects/${projectId}`).send({
+        blah: 'Some text',
+      })
+      const responses = await Promise.all([updateCreatedBy, updateBlah])
+      responses.forEach(response => {
+        const {
+          status,
+          body: { id, errors },
+        } = response
+        expect(status).toBe(400)
+        expect(errors).toEqual(expect.any(Array))
+        expect(id).toBeUndefined()
+      })
+    })
+
+    it('responds with 404 when the project is not found', async () => {
+      const {
+        body: { errors },
+      } = await requestWithAuth
+        .patch(`/projects/1`)
+        .send({ name: 'New Name' })
+        .expect(404)
+      expect(errors).toEqual(expect.any(Array))
+    })
+
+    // TODO: Send Bad Request for missing patch body - currently sends a 500 error
+  })
+
+  describe('delete', () => {
+    it('deletes a project', async () => {
+      const project = new ProjectFixture({
+        name: 'Project W',
+        createdBy: user.id,
+      })
+      const projectAttributes = await project.create()
+      const { id: projectId } = projectAttributes
+
+      const responseWhenRead = await requestWithAuth.get(
+        `/projects/${projectId}`
+      )
+
+      const responseWhenDeleted = await requestWithAuth.del(
+        `/projects/${projectId}`
+      )
+
+      const responseWhenReadAfterDeleted = await requestWithAuth.get(
+        `/projects/${projectId}`
+      )
+
+      expect(responseWhenRead.body).toEqual(projectAttributes)
+      expect(responseWhenDeleted.body).toEqual(projectAttributes)
+      expect(responseWhenReadAfterDeleted.status).toBe(404)
+      expect(responseWhenReadAfterDeleted.body.errors).toEqual(
+        expect.any(Array)
+      )
+    })
+
+    it('responds with 404 when the project is not found', async () => {
+      const {
+        body: { errors },
+      } = await requestWithAuth.del(`/projects/1`).expect(404)
+      expect(errors).toEqual(expect.any(Array))
     })
   })
 })
